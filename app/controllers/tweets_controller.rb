61462f37bae7eec45d7019422183bbff9bc5fb55
tokitsu <toki2222son@yahoo.co.jp>
2021-06-25 16:26:14 +0900

    add association

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,28 @@
 class TweetsController < ApplicationController
   def new
+    @tweet = Tweet.new
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
+
+    @user = @tweet.user
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @tweets = @user.tweets
+
+    @favorite_tweets = @user.favorite_tweets
+  end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.following
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.follower
   end
 end
